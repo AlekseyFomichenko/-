e18a0bd222e4c86743cfa8b1c9e83bc517a77bb3
AlekseyFomichenko <128860587+AlekseyFomichenko@users.noreply.github.com>
2023-12-06 21:00:37 +0300

    homework 4

---
+++ Семинар 4/StudentController.java
@@ -0,0 +1,16 @@
+public class StudentController implements UserController<Student>{
+
+    private final StudentService studentService = new StudentService();
+
+    private final StudentView studentView = new StudentView();
+    @Override
+    public void create(String firstName, String secondName, String lastName) {
+        studentService.create(firstName, secondName, lastName);
+    }
+
+    @Override
+    public void sendOnConsole() {
+        studentView.sendOnConsole(studentService.getStudentList());
+    }
+    
+}
